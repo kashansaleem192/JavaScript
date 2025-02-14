5fe1a308f73006e3100067bdf59e10f70408d867
Kashansaleem192 <kashansaleem192@gmail.com>
2025-02-14 17:33:22 +0500

    Javascript

---
+++ app.js
@@ -0,0 +1,27 @@
+//  Chapter 1
+
+// Q 1
+
+alert("Welcome To Our Website")
+
+//  Q 2
+
+alert( "Error! Please enter a valid password.")
+
+//  Q 3
+
+alert ( "Welcome to JS land... \n Happy Coding")
+
+//  Q 4 
+
+alert ("Welcome to js Land...")
+alert( "Happy Coding!")
+
+// Q 5
+
+console.log("Hello...I can run JS through my web browser's console");
+
+
+
+
+
---
+++ chapter 2/chapter2.js
@@ -0,0 +1,20 @@
+// chapter 2 
+
+// Q 1
+
+var username;
+
+
+// Q 2
+
+// A
+
+var username="Kashan Saleem"
+
+// B
+
+var message="Hello World"
+
+// C
+
+alert ("message")
